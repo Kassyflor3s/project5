b606b896cb462674ca3585d6ffeb7862e5b045be
Kassandra Flores <kassyflores2013@gmail.com>
2024-10-02 15:23:18 -0500

    Login

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="bg-slate-500 h-20 items-center text-white flex justify-center space-x-4">
     <RouterLink :to="{name: 'Home'}">Home</RouterLink>
-    <RouterLink :to="{name: 'AboutMe'}">About Me</RouterLink>
     <RouterLink :to="{name: 'Contact'}">Contact</RouterLink>
+    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
     <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
   </nav>
 </template>
